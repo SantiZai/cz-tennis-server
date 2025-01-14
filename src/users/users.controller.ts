b94b5c5fa25e4dd6003c37e14d4634eedc0eef85
SantiZai <santyagozaidan@gmail.com>
2025-01-14 16:27:40 -0300

    users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  Get,
+  Patch,
+  Post,
+  Delete,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from '@prisma/client';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get('id/:id')
+  async findOneById(@Param('id') id: string): Promise<User | null> {
+    return await this.usersService.getUserById(id);
+  }
+
+  @Get(':email')
+  async findOneByEmail(@Param('email') email: string): Promise<User | null> {
+    return await this.usersService.getUserByEmail(email);
+  }
+
+  @Post()
+  async create(@Body() user: User): Promise<User> {
+    return await this.usersService.createUser(user);
+  }
+
+  @Patch()
+  async update(@Body() user: Partial<User>): Promise<User | null> {
+    return await this.usersService.updateUser(user);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<User | null> {
+    return await this.usersService.deleteUser(id);
+  }
+}
