323b0245a449581dcd18b0248c852159b8497436
SantiZai <santyagozaidan@gmail.com>
2025-01-14 16:02:25 -0300

    changes in the table names

---
+++ prisma/migrations/20250114185757_change/migration.sql
@@ -0,0 +1,67 @@
+/*
+  Warnings:
+
+  - You are about to drop the `orders` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `products` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "orders" DROP CONSTRAINT "orders_user_id_fkey";
+
+-- DropTable
+DROP TABLE "orders";
+
+-- DropTable
+DROP TABLE "products";
+
+-- DropTable
+DROP TABLE "users";
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "address" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "brand" TEXT NOT NULL,
+    "image_url" TEXT,
+    "variants" TEXT[],
+    "categories" TEXT[],
+    "on_offer" BOOLEAN NOT NULL DEFAULT false,
+    "prices" DOUBLE PRECISION[],
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "products" TEXT[],
+    "state" TEXT NOT NULL DEFAULT 'pending',
+    "total" DOUBLE PRECISION NOT NULL,
+    "delivery_address" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
